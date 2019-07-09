de6ae1971265b9a7c8ebb71de3aa9629d4632449
khjobran1 <khaled.j@hungerstation.com>
2019-07-09 10:51:29 +0300

    todo api

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,31 @@
+class TodosController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_action :allow_cros_origin
+
+  def get_all
+    render json: { data: Todo.all }, status: :ok
+  end
+
+  def add_todo
+    @todo = Todo.new(title: params[:title], completed: params[:completed])
+    if @todo.save
+      render json: { msg:'success', data: @todo }, status: :ok
+    else
+      render json: { msg:'failed' }, status: :unproccessable_entity
+    end
+  end
+
+  def destroy
+    Todo.find(params[:id]).destroy
+  end
+
+  def mark_complete
+    Todo.find(params[:id]).update(completed: params[:completed])
+  end
+
+  private
+
+  def allow_cros_origin
+    response.set_header('Access-Control-Allow-Origin', '*')
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'todos' => 'todos#get_all'
+  post 'addtodo' => 'todos#add_todo'
+  post 'destroy' => 'todos#destroy'
+  post 'complete' => 'todos#mark_complete'
+
 end
